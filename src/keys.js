2c264bc75748c922e7b4067b568106fd27373bd4
ZettaPloom <z.ska.santiago@gmail.com>
2020-05-06 08:45:07 -0500

    Keys usage updated

--- src/keys.js
+++ src/keys.js
@@ -1,10 +1,13 @@
+require('dotenv').config({path: 'src/.env'});
+console.log(process.env);
+
 module.exports = {
 
     database: {
         connectionLimit: 10,
-        host: 'localhost',
-        user: 'root',
-        password: '',
+        host: process.env.MY_HOST,
+        user: process.env.MY_USER,
+        password: process.env.MY_PASSWORD,
         database: 'db_smarttheeth'
     }
 
